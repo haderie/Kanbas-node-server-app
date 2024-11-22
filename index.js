bd9e8eaf97802dcb2a7dd1390c799bc654bfd959
haderie <haderie49@gmail.com>
2024-11-22 17:30:31 -0500

    final

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
+
 const app = express();
 app.use(
   cors({
@@ -16,11 +17,13 @@ app.use(
     origin: process.env.NETLIFY_URL || "http://localhost:3000",
   })
 );
+
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
@@ -30,6 +33,7 @@ if (process.env.NODE_ENV !== "development") {
   };
 }
 app.use(session(sessionOptions));
+
 app.use(express.json());
 UserRoutes(app);
 CourseRoutes(app);
