3bb1d510f1f13cbb88ade05d19d6ed403b75e8b6
haderie <haderie49@gmail.com>
2024-11-22 17:04:10 -0500

    assignemnts

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
-import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
-
-import session from "express-session";
+import cors from "cors";
 import "dotenv/config";
-
+import session from "express-session";
 const app = express();
 app.use(
   cors({
@@ -17,7 +16,6 @@ app.use(
     origin: process.env.NETLIFY_URL || "http://localhost:3000",
   })
 );
-
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
@@ -32,12 +30,12 @@ if (process.env.NODE_ENV !== "development") {
   };
 }
 app.use(session(sessionOptions));
-
 app.use(express.json());
 UserRoutes(app);
 CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
 EnrollmentRoutes(app);
 Lab5(app);
 Hello(app);
-ModuleRoutes(app);
 app.listen(process.env.PORT || 4000);
