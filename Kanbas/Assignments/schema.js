dc854b04e1cc7c707b581ace155b6afe5f54cf71
haderie <haderie49@gmail.com>
2024-12-22 11:18:25 -0500

    final changes

---
+++ Kanbas/Assignments/schema.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const assignmentSchema = new mongoose.Schema(
+  {
+    title: String,
+    course: { type: mongoose.Schema.Types.ObjectId, ref: "CourseModel" },
+    description: String,
+    availableFromDate: String,
+    availableUntilDate: String,
+    due: String,
+    points: String,
+  },
+  { collection: "assignments" }
+);
+export default assignmentSchema;
